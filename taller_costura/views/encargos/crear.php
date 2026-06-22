b87a3f57a5dc36186ab1e6c29fd67c646f0d82df
Caro <gcfetta@gmail.com>
2026-06-22 15:21:55 -0300

    actualizo mensaje otra vezz

--- taller_costura/views/encargos/crear.php
+++ taller_costura/views/encargos/crear.php
@@ -100,7 +100,6 @@ if (!empty($_POST['cliente_id'])) {
           <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="12" y1="1" x2="12" y2="23"></line><path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path></svg>
           Información de Pago
         </h3>
-        <p style="margin-bottom:18px;">Obligatorio.</p>
 
         <div class="form-group">
           <label for="monto_total">Precio Total</label>
