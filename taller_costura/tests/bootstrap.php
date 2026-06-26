a401e90fb8ce711ba5bb8ec3c7362048c499fee0
Delfina ibanez <delfinaibanezgiordano@gmail.com>
2026-06-26 01:55:17 -0300

    correccion test con ruta relativa

--- taller_costura/tests/bootstrap.php
+++ taller_costura/tests/bootstrap.php
@@ -1,8 +1,5 @@
 <?php
-// tests/bootstrap.php
-// Carga las clases del sistema sin arrancar la aplicación completa
-
-define('BASE_PATH', 'C:/wamp64/www/sistema_costura/grupo-06/taller_costura');
+define('BASE_PATH', dirname(__DIR__));
 
 require_once BASE_PATH . '/vendor/autoload.php';
 
